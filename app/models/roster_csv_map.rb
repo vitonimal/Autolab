c0609140f0dc5cea1fb0d106774385bef3a2defd
xinyis991105 <xinyis@andrew.cmu.edu>
2020-04-29 22:24:07 -0400

    further commented roster_csv_map model

--- app/models/roster_csv_map.rb
+++ app/models/roster_csv_map.rb
@@ -1,4 +1,10 @@
 class RosterCsvMap < ApplicationRecord
   self.table_name = "roster_csv_map"
-
+  
+  # I assume we need to pass in the sortable table information here?
+  def self.create()
+  	newMap = RosterCsvMap.new()
+  	# and then complete the initialization of the new map?
+  	return newMap
+  end
 end
