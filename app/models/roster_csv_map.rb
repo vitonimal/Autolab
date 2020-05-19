dad1511ec79350b861dc2616dcc3f05ab3b2e2ff
xinyis991105 <xinyis@andrew.cmu.edu>
2020-05-19 11:32:12 -0400

    added code for creating a new roster csv map

--- app/models/roster_csv_map.rb
+++ app/models/roster_csv_map.rb
@@ -1,10 +1,23 @@
 class RosterCsvMap < ApplicationRecord
   self.table_name = "roster_csv_map"
   
-  # I assume we need to pass in the sortable table information here?
-  def self.create()
+  def self.create(courseName)
   	newMap = RosterCsvMap.new()
-  	# and then complete the initialization of the new map?
+
+  	newMap.name = "#{courseName} Roster Map"
+
+  	# Set the default values of each column
+  	newMap.semestercol = 0
+    newMap.emailcol = 1
+    newMap.lastnamecol = 2
+    newMap.firstnamecol = 3
+    newMap.schoolcol = 4
+    newMap.majorcol = 5
+    newMap.yearcol = 6
+    newMap.gradingpolicycol = 7
+    newMap.coursenumbercol = 8
+    newMap.courselecturecol = 9
+    newMap.sectioncol = 10
   	return newMap
   end
 end
